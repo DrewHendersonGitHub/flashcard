124e2bfa1073b96cb2d65d95f7723b2738201ad9
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-19 14:34:54 -0700

    added more flashcards
    Co-authored-by: Chase Stokes <chasestokes@gmail.com>
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,57 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  
+  $("#id1").click(function() {
     $("#id1").toggle();
     $("#id1D").toggle();
   });
+  $("#id1D").click(function() {
+    $("#id1").toggle();
+    $("#id1D").toggle();
+  });
+
+  $("#id2").click(function() {
+    $("#id2").toggle();
+    $("#id2D").toggle();
+  });
+  $("#id2D").click(function() {
+    $("#id2").toggle();
+    $("#id2D").toggle();
+  });
+
+  $("#id3").click(function() {
+    $("#id3").toggle();
+    $("#id3D").toggle();
+  });
+  $("#id3D").click(function() {
+    $("#id3").toggle();
+    $("#id3D").toggle();
+  });
+
+  $("#id4").click(function() {
+    $("#id4").toggle();
+    $("#id4D").toggle();
+  });
+  $("#id4D").click(function() {
+    $("#id4").toggle();
+    $("#id4D").toggle();
+  });
+
+  $("#id5").click(function() {
+    $("#id5").toggle();
+    $("#id5D").toggle();
+  });
+  $("#id5D").click(function() {
+    $("#id5").toggle();
+    $("#id5D").toggle();
+  });
+
+  $("#id6").click(function() {
+    $("#id6").toggle();
+    $("#id6D").toggle();
+  });
+  $("#id6D").click(function() {
+    $("#id6").toggle();
+    $("#id6D").toggle();
+  });
+  
 });
